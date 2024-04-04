072c56dbe6b4fff08399b1027a684763710aff32
Manish Singh <prataponandroid@gmail.com>
2024-04-04 14:57:12 +0200

    changed to record

--- src/main/java/com/music/v4/AVCommand.java
+++ src/main/java/com/music/v4/AVCommand.java
@@ -2,33 +2,18 @@ package com.music.v4;
 
 import java.util.Objects;
 
-/**
- * Not specially made but this must be treated as Immutable!!
- */
-public class AVCommand {
-    private String oprFile;
-    private String cmd;
+public record AVCommand(String oprFile, String cmd) {
 
     public AVCommand(String infile) {
-        this.oprFile = Objects.requireNonNull(infile);
-        this.cmd = "";
-    }
-
-    public String getCmd() {
-        return cmd;
-    }
-
-    public String getOprFile() {
-        return oprFile;
+        this(Objects.requireNonNull(infile), "");
     }
 
     public AVCommand newAVCommand(String outFile, String addCmd) {
-        AVCommand avCommand = new AVCommand(outFile);
-        avCommand.cmd = new StringBuilder()
+        String cmd = new StringBuilder()
                 .append(this.cmd)
                 .append("\n")
                 .append(Objects.requireNonNull(addCmd))
                 .toString();
-        return avCommand;
+        return new AVCommand(outFile, cmd);
     }
 }
